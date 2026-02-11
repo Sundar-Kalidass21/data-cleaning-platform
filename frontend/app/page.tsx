6cecc3888a1bc8d18042be64d5a94765a8e7f89c
sundar kalidass <sundar_kalidass@outlook.com>
2026-02-11 20:32:30 +0530

    sick

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-red-500 text-white text-4xl">
-      Tailwind v4 is working
+      Tailwind v4 is workings
     </div>
   )
 }
